136d1c64d17f0eabc34842377a171959e9a3e0af
Tim Michael Heinz Wolf <tim.michael.heinz.wolf@cern.ch>
2019-09-09 06:29:08 -0500

    update after generation of minitrees

--- LoadData.py
+++ LoadData.py
@@ -61,7 +61,7 @@ def GetUniqueTagsOfDataset(dataset):
     return dataset.unique().tolist()
 
 
-def ConfigurePax():
+def ConfigurePax(parsed_args, parsed_config):
     pax_config = configuration.load_configuration('XENON1T')
     n_channels = pax_config['DEFAULT']['n_channels']
     pmts = pax_config['DEFAULT']['pmts']
@@ -69,7 +69,7 @@ def ConfigurePax():
     tpc_radius = pax_config['DEFAULT']['tpc_radius']
     gains = pax_config['DEFAULT']['gains']
 
-    pax_version = '6.10.1'
+    pax_version = parsed_config["pax_settings"]["pax_version"]
     minitree_paths = ['/home/twolf/scratch-midway2', '/dali/lgrandi/xenon1t/minitrees/pax_v'+pax_version, '/project2/lgrandi/xenon1t/minitrees/pax_v'+pax_version, '/project/lgrandi/xenon1t/minitrees/pax_v'+pax_version]
     print(minitree_paths)
 
@@ -115,7 +115,7 @@ def SubmitToCluster(splitted_arrays, pax_settings, parsed_config, parsed_args):
     text = """#!/bin/bash
 #SBATCH --job-name=part{part_id}_of_{splits}_{config}
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=8000
 #SBATCH --output={logfile}
 #SBATCH --error={logfile}
@@ -133,9 +133,10 @@ cp {config} ${{TMPDIR}}
 cd ${{TMPDIR}}
 ls -ltrha
 rm -f pax_event_class*
-source activate pax_head
-python LoadData.py --config {config} --load --runs {runs} --part_id {part_id} --splits {splits}
-echo "python LoadData.py --config {config} --load --runs {runs} --part_id {part_id} --splits {splits}"
+#  source activate pax_head
+source activate pax_v6.10.1
+python LoadData.py --config {config_in_tmp} --load --runs {runs} --part_id {part_id} --splits {splits}
+echo "python LoadData.py --config {config_in_tmp} --load --runs {runs} --part_id {part_id} --splits {splits}"
 ls -ltrha
 mv *.pkl {outdir}
 
@@ -143,8 +144,10 @@ mv *.pkl {outdir}
     for part_id, runs in enumerate(splitted_arrays):
         logfile = GetFilename(parsed_config, part_id, parsed_args["splits"])
         logfile = logfile.replace(".pkl", ".log")
+        config_in_tmp = os.path.basename(parsed_args["config"])
         runs_string = "'" +  "' '".join(runs) + "'"
-        y = text.format(config=parsed_args["config"], runs=runs_string, part_id=part_id, splits=parsed_args['splits'], qos=parsed_args['qos'], partition=parsed_args['partition'], logfile=logfile, outdir=parsed_config["outdir"])
+        y = text.format(config=parsed_args["config"], runs=runs_string, part_id=part_id, splits=parsed_args['splits'], qos=parsed_args['qos'], partition=parsed_args['partition'], logfile=logfile, outdir=parsed_config["outdir"],
+            config_in_tmp=config_in_tmp)
         submit_job(y)
 
 
@@ -216,7 +219,11 @@ def main(arg1):
   parsed_args, parsed_config = parse_args(arg1)
   pax_settings = parsed_config["pax_settings"]
 
-  ConfigurePax()
+  ConfigurePax(parsed_args, parsed_config)
+
+  if parsed_args['runs'] is not None:
+      PicklePerRuns(parsed_args["part_id"], parsed_args["splits"], parsed_args["runs"], pax_settings, parsed_config, parsed_args)
+      raise SystemExit
 
   datasets = hax.runs.datasets # this variable holds all dataset info
 
@@ -239,9 +246,6 @@ def main(arg1):
   dsets_type = SelectDataAccordingToType(parsed_config, pax_settings, dsets, datasets)
   print("Tags of selected data-sets are: ", dsets_type.tags.unique())
 
-  if parsed_args['runs'] is not None:
-      PicklePerRuns(parsed_args["part_id"], parsed_args["splits"], parsed_args["runs"], pax_settings, parsed_config, parsed_args)
-      raise SystemExit
 
   if parsed_args['debug']:
     run_names = dsets_type["name"].tolist()[:10]
